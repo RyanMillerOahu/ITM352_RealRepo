3f33049d847e9c7783b1740114bcf360b26bc690
RyanMillerOahu <mr75@hawaii.edu>
2019-10-03 16:06:28 -1000

    Update Ex1.js

--- Lab 8/Ex1.js
+++ Lab 8/Ex1.js
@@ -1,13 +1,6 @@
 var age = 20;
-var number = 0; 
-while ( number < age ) {
- 
- number++;
- if(number > (age*0.5) ) {
-     console.log("Dont ask how old I am");
-     process.exit();
- }
- console.log(`Are you ${number} years old?`);
+for (var number = 50; number < age; number--) {
+     console.log(`Are you ${number} years old?`);
 }
 console.log(`You must be ${number} years old!`);
 
